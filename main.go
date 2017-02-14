e5cd9bdc29f2086ce39f6855f02af132be30b988
Adam Ryman <adamryman@gmail.com>
2017-02-13 20:31:53 -0800

    Messing around

--- main.go
+++ main.go
@@ -4,16 +4,23 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
 
+	"github.com/adamryman/gophersay/gopherart"
 	"github.com/nlopes/slack"
+
+	_ "github.com/joho/godotenv/autoload"
 )
 
-import _ "github.com/joho/godotenv/autoload"
-
 const prPartyID = "C3YJF4GP5"
+const playgroundId = "C03LPQF0Y"
 
 func main() {
 	token := os.Getenv("SLACK_API_TOKEN")
+	gopher, _ := gopherart.Asset("gopherart/gopher.ascii")
 
 	api := slack.New(token)
 	logger := log.New(os.Stdout, "slack-bot: ", log.Lshortfile|log.LstdFlags)
@@ -22,6 +29,14 @@ func main() {
 
 	rtm := api.NewRTM()
 	go rtm.ManageConnection()
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		<-c
+		rtm.SendMessage(rtm.NewOutgoingMessage("TERMINATED", playgroundId))
+		time.Sleep(time.Second)
+		os.Exit(0)
+	}()
 
 	for msg := range rtm.IncomingEvents {
 		fmt.Print("Event Received: ")
@@ -35,10 +50,31 @@ func main() {
 			fmt.Println("Infos:", ev.Info)
 			fmt.Println("Connection counter:", ev.ConnectionCount)
 			// Replace #general with your Channel ID
-			rtm.SendMessage(rtm.NewOutgoingMessage("Hello world!", prPartyID))
+			rtm.SendMessage(rtm.NewOutgoingMessage("Rebooting....Done "+time.Now().String(), playgroundId))
 
 		case *slack.MessageEvent:
 			fmt.Printf("Message: %v\n", ev)
+			txt := ev.Text
+
+			if strings.HasPrefix(txt, "gophersay") {
+				txt = strings.TrimPrefix(txt, "gophersay ")
+				message := fmt.Sprintf("```\n%s\n%s\n%s\n```\n",
+					" ------------------------",
+					txt,
+					gopher,
+				)
+				rtm.SendMessage(rtm.NewOutgoingMessage(message, playgroundId))
+			}
+
+			if strings.Contains(txt, "<@U456ZSLSJ>") {
+				newtxt := strings.Replace(txt, "<@U456ZSLSJ>", "", -1)
+				rtm.SendMessage(rtm.NewOutgoingMessage(Reverse(newtxt), playgroundId))
+			}
+
+			//if ev.User == "U03L3L1NC" {
+			//count := strings.Count(txt, " ")
+			//rtm.SendMessage(rtm.NewOutgoingMessage(fmt.Sprintf("John said %d words", count), playgroundId))
+			//}
 
 		case *slack.PresenceChangeEvent:
 			fmt.Printf("Presence Change: %v\n", ev)
@@ -59,4 +95,12 @@ func main() {
 			// fmt.Printf("Unexpected: %v\n", msg.Data)
 		}
 	}
+}
+
+func Reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
 }
